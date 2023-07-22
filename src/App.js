90c73893bae1753f067ae033aef655375e7f8e90
potlitel <potlitel@gmail.com>
2023-07-22 12:22:23 -0400

    Footer component is created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Analytics from "./components/Analytics";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Newsletters from "./components/Newsletters";
@@ -12,6 +13,7 @@ function App() {
       <Analytics />
       <Newsletters />
       <Cards />
+      <Footer />
     </div>
   );
 }
