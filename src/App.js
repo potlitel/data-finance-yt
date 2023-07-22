5c41102e479b0ae69e9a7ed45fb81277a9a8f491
potlitel <potlitel@gmail.com>
2023-07-22 09:51:15 -0400

    Newsletter component is created.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Analytics from "./components/Analytics";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Newsletters from "./components/Newsletters";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletters />
     </div>
   );
 }
